327fc0376b7f8ff226033794d0b081a0c8d84590
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2019-08-20 01:59:54 -0400

    made difficulty buttons responsive

---
+++ src/JavaScript/dificulty.js
@@ -0,0 +1,20 @@
+$('#btn-easy').click(function(){
+    $('.easy').show();
+    $('.standard').hide();
+    $('.hard').hide();
+
+   });
+
+   $('#btn-standard').click(function(){
+    $('.standard').show();
+    $('.easy').hide();
+    $('.hard').hide();
+
+   });
+
+   $('#btn-hard').click(function(){
+    $('.hard').show();
+    $('.standard').hide();
+    $('.easy').hide();
+
+   });
