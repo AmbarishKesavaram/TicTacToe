d813b404ca641eb8c440964250d739a5218f583b
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2019-08-20 15:48:20 -0400

    working game

--- src/JavaScript/dificulty.js
+++ src/JavaScript/dificulty.js
@@ -1,20 +1,14 @@
-$('#btn-easy').click(function(){
-    $('.easy').show();
-    $('.standard').hide();
-    $('.hard').hide();
 
-   });
-
-   $('#btn-standard').click(function(){
-    $('.standard').show();
-    $('.easy').hide();
-    $('.hard').hide();
-
-   });
-
-   $('#btn-hard').click(function(){
-    $('.hard').show();
-    $('.standard').hide();
-    $('.easy').hide();
-
-   });
+   function easy_click(){
+    document.getElementById('difficulty').innerText = 'easy';
+    
+   }
+   function standard_click(){
+    document.getElementById('difficulty').innerText = 'standard';
+   }
+   
+   function hard_click(){
+    document.getElementById('difficulty').innerText = 'hard';
+   }
+   
+  
